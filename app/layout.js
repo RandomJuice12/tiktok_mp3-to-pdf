b13c4cf63c4fdc30be62e25977f1269c9f920172
RandomJuice12 <RandomJuice121@outlook.com>
2025-11-23 10:12:24 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,18 @@ import { Analytics } from '@vercel/analytics/react';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}
-        <Analytics />
+      <head>
+        {/* Umami Analytics – starts tracking immediately */}
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="214893a7-ce04-44fe-acb1-1fabd5cab8b6"
+        ></script>
+      </head>
+
+      <body>
+        {children}
+        <Analytics />  {/* ← you can delete this line later if you want */}
       </body>
     </html>
   )
